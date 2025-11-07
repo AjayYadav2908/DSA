a3fa8440aed9d9317f860a43117125e6a7c0787c
AjayYadav2908 <ajayydv2908@gmail.com>
2025-11-07 11:54:30 +0530

    Time: 11 ms (29.11%) | Memory: 127.8 MB (69.09%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,28 +2,57 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int>ans(n);
-        int j=0;
+        // brute force
+        // vector<int>ans(n);
+        // int j=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot){
+        //        ans[j]=nums[i];
+        //        j++; 
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==pivot){
+        //        ans[j]=nums[i];
+        //        j++; 
+        //     }
+        // }
+        //   for(int i=0;i<n;i++){
+        //     if(nums[i]>pivot){
+        //        ans[j]=nums[i];
+        //        j++; 
+        //     }
+        // }
+        // return ans ;
+        vector<int> res(n,0);
+
+        int countless =0;
+        int countequal =0;
+        for(int i =0;i<n;i++){
+            if(nums[i]<pivot)countless++;
+            else if(nums[i]==pivot)countequal++;
+        }
+        int x=0;
+        int j = countless;
+        int k = countless+countequal;
+        
         for(int i=0;i<n;i++){
             if(nums[i]<pivot){
-               ans[j]=nums[i];
-               j++; 
+                res[x]=nums[i];
+                x++;
+            }
+            else if(nums[i]==pivot){
+                res[j]=nums[i];
+                j++;
+            }
+            else if(nums[i]>pivot) {
+                res[k]= nums[i];
+                k++;
             }
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]==pivot){
-               ans[j]=nums[i];
-               j++; 
-            }
-        }
-          for(int i=0;i<n;i++){
-            if(nums[i]>pivot){
-               ans[j]=nums[i];
-               j++; 
-            }
-        }
-        return ans ;
-        
+        return res;
+
+       
 
         
     }
