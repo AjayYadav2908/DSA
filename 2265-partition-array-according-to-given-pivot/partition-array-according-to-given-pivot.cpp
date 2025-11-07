d17a3fdbf8edece48154ada06134377a03f3fd2c
AjayYadav2908 <ajayydv2908@gmail.com>
2025-11-07 11:55:09 +0530

    Time: 12 ms (23.04%) | Memory: 127.8 MB (76.24%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,55 +2,55 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        // brute force
-        // vector<int>ans(n);
-        // int j=0;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]<pivot){
-        //        ans[j]=nums[i];
-        //        j++; 
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==pivot){
-        //        ans[j]=nums[i];
-        //        j++; 
-        //     }
-        // }
-        //   for(int i=0;i<n;i++){
-        //     if(nums[i]>pivot){
-        //        ans[j]=nums[i];
-        //        j++; 
-        //     }
-        // }
-        // return ans ;
-        vector<int> res(n,0);
-
-        int countless =0;
-        int countequal =0;
-        for(int i =0;i<n;i++){
-            if(nums[i]<pivot)countless++;
-            else if(nums[i]==pivot)countequal++;
-        }
-        int x=0;
-        int j = countless;
-        int k = countless+countequal;
-        
+        //brute force
+        vector<int>ans(n);
+        int j=0;
         for(int i=0;i<n;i++){
             if(nums[i]<pivot){
-                res[x]=nums[i];
-                x++;
-            }
-            else if(nums[i]==pivot){
-                res[j]=nums[i];
-                j++;
-            }
-            else if(nums[i]>pivot) {
-                res[k]= nums[i];
-                k++;
+               ans[j]=nums[i];
+               j++; 
             }
         }
-        return res;
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot){
+               ans[j]=nums[i];
+               j++; 
+            }
+        }
+          for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+               ans[j]=nums[i];
+               j++; 
+            }
+        }
+        return ans ;
+        // vector<int> res(n,0);
+
+        // int countless =0;
+        // int countequal =0;
+        // for(int i =0;i<n;i++){
+        //     if(nums[i]<pivot)countless++;
+        //     else if(nums[i]==pivot)countequal++;
+        // }
+        // int x=0;
+        // int j = countless;
+        // int k = countless+countequal;
+        
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot){
+        //         res[x]=nums[i];
+        //         x++;
+        //     }
+        //     else if(nums[i]==pivot){
+        //         res[j]=nums[i];
+        //         j++;
+        //     }
+        //     else if(nums[i]>pivot) {
+        //         res[k]= nums[i];
+        //         k++;
+        //     }
+        // }
+        // return res;
 
        
 
