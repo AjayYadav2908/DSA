15e53148ef15384bbf321e4a2e4077a8dfc0732c
AjayYadav2908 <ajayydv2908@gmail.com>
2025-11-13 23:55:50 +0530

    Time: 25 ms (30.48%) | Memory: 17.4 MB (55.03%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,10 +5,10 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
 
-        for (int i = 0; i < n - 3; i++) {
+        for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
 
-            for (int j = i + 1; j < n - 2; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate j
 
                 int p = j + 1;
